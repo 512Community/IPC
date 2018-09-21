57978e558b01f11c2b27dfbed45a2fe268fd5d0a
Qin TianLong <qintl@yytek.com>
2018-09-21 11:07:39 +0800

    msg_log.h 添加 extern C
    Signed-off-by: Qin TianLong <qintl@yytek.com>

--- msg_log/include/msg_log.h
+++ msg_log/include/msg_log.h
@@ -1,6 +1,14 @@
 #ifndef __MSG_LOG_H__
 #define __MSG_LOG_H__
-void ESLOG_ERR(const char *szFormat, ...);
-void ESLOG_INFO(const char *szFormat, ...);
-void ESLOG_BIN(const char * name,char * data,unsigned int len);
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+extern void ESLOG_ERR(const char *szFormat, ...);
+extern void ESLOG_INFO(const char *szFormat, ...);
+extern void ESLOG_BIN(const char * name,char * data,unsigned int len);
+
+#ifdef __cplusplus
+}
+#endif
 #endif
